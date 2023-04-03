14e0982f71e24a567a90ee3c073c7efe8a1d1144
Yanis Falaki <yanisfalaki@outlook.com>
2023-04-03 01:55:55 -0400

    Added pseudocode

--- centris-integration.py
+++ centris-integration.py
@@ -8,7 +8,29 @@ def Start():
       raise ValueError("DB_PATH environment variable not set")
   conn = sqlite3.connect(db_path)
 
+  # Query database INSCRIPTIONS table
 
+  # for every property
+    # Get the property title by concatenating address number and street
+    # Add description
+        # Add english addenda from inscription
+        # Add french addenda from inscriptiom
+    # Find property type
+    # Get sale or rent status
+    # get price
+    # get number of bedrooms
+    # get number of bathrooms
+    # get area size
+    # get property ID
+    # get year built
+    # add location info:
+        # Address (Same as property title)
+        # Country in this case canada
+        # City; get from municipalities table
+        # postal code
+
+  for row in cursor2:
+    print(row)
 
 if __name__ == "__main__":
   Start()
