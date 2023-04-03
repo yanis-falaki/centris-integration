7ff25c32646cc3f3fab41267cf33da91291d31df
Yanis Falaki <yanisfalaki@outlook.com>
2023-04-02 21:18:38 -0400

    Added base code and files

---
+++ centris-integration.py
@@ -0,0 +1,15 @@
+import os
+import sqlite3
+import requests
+
+def Start():
+  db_path = os.environ.get('DB_PATH')
+  if db_path is None:
+      raise ValueError("DB_PATH environment variable not set")
+
+  conn = sqlite3.connect(db_path)
+
+
+
+if __name__ == "__main__":
+  Start()
