9cd561968e19e1589df100afd2b7c81d5ce3f79e
Yanis Falaki <2005yanisf@gmail.com>
2023-04-07 19:56:22 -0400

    Plugin now takes input

--- centris-integration-plugin/centris-integration.php
+++ centris-integration-plugin/centris-integration.php
@@ -23,9 +23,43 @@ add_action('admin_menu', 'cenris_integration_add_menu_page');
 
 function centris_integration_display_settings_page() {
   ?>
-  <h1>Centris Integration Settings</h1>
-  
+  <h1>Centris Integration</h1>
+  <form method="post" action="">
+    <label for="folder-path">Folder Path:</label>
+    <input type="text" id="folder-path" name="folder_path" value="<?php echo get_user_input()["path"] ?>">
 
+    <label for="frequency">Frequency:</label>
+    <select id="frequency" name="frequency">
+      <option value="daily" <?php if ('daily' == get_user_input()["frequency"]) echo 'selected'; ?>>Daily</option>
+      <option value="weekly" <?php if ('weekly' == get_user_input()["frequency"]) echo 'selected'; ?>>Weekly</option>
+    </select>
+
+    <input type="submit" name="submit" value="Submit">
+  </form>
+  <section>
+    <h2>Last Updates:</h2>
+  </section>
   <?php
+}
+
+function save_user_input() {
+  if (isset($_POST['frequency'])) {
+      $frequency = sanitize_text_field($_POST['frequency']);
+      update_option('frequency_option', $frequency);
+  }
+  if (isset($_POST['folder_path'])) {
+    $path = sanitize_text_field($_POST['folder_path']);
+    update_option('passerelle_path_option', $path);
+  }
+}
+
+function get_user_input() {
+  $frequency = get_option('frequency_option', '');
+  $path = get_option('passerelle_path_option', '');
+  return ["frequency" => $frequency, "path" => $path];
+}
+
+if (isset($_POST['submit'])) {
+  save_user_input();
 }
 ?>
